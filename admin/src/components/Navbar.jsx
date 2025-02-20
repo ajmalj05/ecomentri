6c7a8a29a3c2762e1f35919ebfe08f9972afd4d4
ajmalj05 <ajmalj2003@gmail.com>
2025-02-20 11:48:48 +0530

    Initial project setup: add core files

--- admin/src/components/Navbar.jsx
+++ admin/src/components/Navbar.jsx
@@ -3,7 +3,7 @@ import { assets } from "../assets/assets";
 const Navbar = ({ setToken }) => {
   return (
     <div className="flex items-center py-2 px-[4%] justify-between">
-      <img src={assets.logo} style={{ width: "max(10%, 80px)" }} alt="" />
+      <h1 className="text-2xl font-bold text-gray-800">The Store</h1>
       <button
         onClick={() => setToken("")}
         className="bg-gray-600 text-white px-5 py-5 sm:px-7 sm:py-2 rounded-full text-xs sm:text-sm"
