38fc6ec715e6c87b56d674dc86ad41832b7a08af
ajmalj05 <ajmalj2003@gmail.com>
2025-02-20 12:35:59 +0530

    Initial project setup: add core files

--- backend/server.js
+++ backend/server.js
@@ -24,7 +24,7 @@ app.use("/api/cart", cartRouter);
 app.use("/api/order", orderRouter);
 
 app.get("/", (req, res) => {
-  res.send("API WORKING");
+  res.send("IT'S WORKING WOWWWW");
 });
 
 app.listen(port, () => {
