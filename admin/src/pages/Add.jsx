f3ceb517afd1c076af6789b839057ccfe6679ea1
ajmalj05 <ajmalj2003@gmail.com>
2025-02-20 13:19:33 +0530

    Initial project setup: add core files

--- admin/src/pages/Add.jsx
+++ admin/src/pages/Add.jsx
@@ -198,7 +198,7 @@ const Add = ({ token }) => {
                 </label>
                 <div className="relative">
                   <div className="absolute inset-y-0 left-0 pl-3 flex items-center pointer-events-none">
-                    <InrSign className="h-5 w-5 text-gray-400" />
+                    <DollarSign className="h-5 w-5 text-gray-400" />
                   </div>
                   <input
                     type="number"
