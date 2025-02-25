57dd5a3454ac2b0f8984060ecde072429ed6b229
Ajmalj12 <ajmaljs2003@gmail.com>
2025-02-25 14:57:33 +0530

    Initial project setup: add core files

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -92,7 +92,7 @@ const Login = () => {
       />
 
       <div className="w-full flex justify-between text-sm mt-[-8px]">
-        <p className="cursor-pointer">Forgot your password?</p>
+        
         {currentState === "Login" ? (
           <p
             onClick={() => setCurrentState("Sign Up")}
